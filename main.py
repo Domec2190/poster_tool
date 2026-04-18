3822b4605701732e1f3263ada33d4d7c2cb5fbc7
Domec2190 <dominik.pavelek2190@gmail.com>
2026-04-18 15:38:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ data = {
 "signatures": [s.strip() for s in signatures.split(",") if s.strip()]
 }
 
+```
     path = render_template("official_notice", data)
 
     # cache busting (?t=...) aby sa obrázok vždy obnovil
@@ -37,4 +38,4 @@ data = {
 
 except Exception as e:
     return f"<pre>Error: {str(e)}</pre>"
-
+```
